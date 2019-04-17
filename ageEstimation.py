a541dfc37c91004f472580bb60ea8dd6de2c9487
xzhao229 <xzhao229@wisc.edu>
2019-04-17 01:00:41 -0400

    Age Prediction Done

---
+++ ageEstimation.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+import cv2
+import dlib
+import numpy as np
+from wide_resnet import WideResNet
+from keras.utils.data_utils import get_file
+
+pretrained_model = "pretrained_models/weights.28-3.73.hdf5"
+modhash = 'fbe63257a054c1c5466cfd7bf14646d6'
+
+
+def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX,
+               font_scale=0.8, thickness=1):
+    size = cv2.getTextSize(label, font, font_scale, thickness)[0]
+    x, y = point
+    cv2.rectangle(image, (x, y - size[1]), (x + size[0], y), (255, 0, 0), cv2.FILLED)
+    cv2.putText(image, label, point, font, font_scale, (255, 255, 255), thickness, lineType=cv2.LINE_AA)
+
+
+def yield_images_from_dir(image_dir):
+    image_dir = Path(image_dir)
+
+    for image_path in image_dir.glob("*.*"):
+        img = cv2.imread(str(image_path), 1)
+
+        if img is not None:
+            h, w, _ = img.shape
+            r = 640 / max(w, h)
+            yield cv2.resize(img, (int(w * r), int(h * r)))
+
+
+def main():
+    # args = get_args()
+    depth = 16
+    k = 8
+    margin = 0.4
+    image_dir = "current_test"
+
+    weight_file = get_file("weights.28-3.73.hdf5", pretrained_model, cache_subdir="pretrained_models",
+                               file_hash=modhash, cache_dir=str(Path(__file__).resolve().parent))
+
+    # for face detection
+    detector = dlib.get_frontal_face_detector()
+
+    # load model and weights
+    img_size = 64
+    model = WideResNet(img_size, depth=depth, k=k)()
+    model.load_weights(weight_file)
+
+    image_generator = yield_images_from_dir(image_dir) if image_dir else yield_images()
+
+    for img in image_generator:
+        input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_h, img_w, _ = np.shape(input_img)
+
+        # detect faces using dlib detector
+        detected = detector(input_img, 1)
+        faces = np.empty((len(detected), img_size, img_size, 3))
+
+        if len(detected) > 0:
+            for i, d in enumerate(detected):
+                x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+                xw1 = max(int(x1 - margin * w), 0)
+                yw1 = max(int(y1 - margin * h), 0)
+                xw2 = min(int(x2 + margin * w), img_w - 1)
+                yw2 = min(int(y2 + margin * h), img_h - 1)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                # cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
+                faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
+
+            # predict ages of the detected faces
+            results = model.predict(faces)
+            ages = np.arange(0, 101).reshape(101, 1)
+            predicted_ages = results[1].dot(ages).flatten()
+
+            # draw results
+            for i, d in enumerate(detected):
+                label = "{}".format(int(predicted_ages[i]))
+                draw_label(img, (d.left(), d.top()), label)
+
+        return img
+
+
+if __name__ == '__main__':
+    main()
--- kivyMain.py
+++ kivyMain.py
@@ -1,19 +1,15 @@
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.graphics.texture import Texture
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.scatter import Scatter
-from kivy.uix.image import Image
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
+import ageEstimation as predict
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.camera import Camera
 import cv2
 
 
 Builder.load_string('''
-    
+
 <RootWidget>
 
     canvas.before:
@@ -28,7 +24,7 @@ Builder.load_string('''
         color: 0,0,0,1
         pos_hint: {'x':0.35, 'top':0.95}
         valign: 'top'
-        
+
     Button:
         id:captureAndTest
         text: 'Test Your Age'
@@ -38,7 +34,7 @@ Builder.load_string('''
         size_hint: None, None
         haligh: 'left'
         pos_hint: {'x':0.349, 'top':0.2}
-        on_release: camera.play = False
+        on_release: camera.play = False, root.capture()
     Button:
         id:recap
         text:'Take Another One'
@@ -48,27 +44,31 @@ Builder.load_string('''
         size_hint: None, None
         pos_hint: {'x':0.505, 'top':0.2}
         on_release: camera.play = True
-    
+
     Camera:
         id: camera
-        resolution: 1080,1080
+        resolution: 800,680
 
-    
+
 ''')
 
 
 class RootWidget(FloatLayout):
     def capture(self):
-        pass
+
+        camera = self.ids['camera']
+        camera.export_to_png("current_test/test_1.jpg")
+        test_result = predict.main()
+        cv2.imshow("result", test_result)
+
+
+
 
 class MainApp(App):
-
-    def build(self):
-        temp = RootWidget()
-        return temp
-
-
+	def build(self):
+		temp = RootWidget()
+		return temp
 
 
 if __name__ == '__main__':
-    MainApp().run()
+	MainApp().run()
