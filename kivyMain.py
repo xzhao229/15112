6f85a40b7298af1fa703775b8762b407bc68f4ba
xzhao229 <49699371+xzhao229@users.noreply.github.com>
2019-04-16 23:54:33 -0400

    Add files via upload

--- kivyMain.py
+++ kivyMain.py
@@ -8,72 +8,67 @@ from kivy.uix.image import Image
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
 import cv2
 
 
-
 Builder.load_string('''
-<BackPacitureLayout>
-    canvas.before:
-        BorderImage:
-            # BorderImage behaves like the CSS BorderImage
-            # border: 10, 10, 10, 10
-            texture: self.background_image.texture
-            pos: self.pos
-            size: self.size
-            
     
 <RootWidget>
-    BackPacitureLayout:
-        # size_hint: .9, .9
-        pos_hint: {'center_x': .45, 'center_y': .5}
-        rows:1
+
+    canvas.before:
+        BorderImage:
+            source: 'bgp.jpg'
+            pos: self.pos
+            size: self.size
+
     Label:
         text: "Mason can tell your age"
         text_size: self.width-20, self.height-20
         color: 0,0,0,1
-        pos_hint: {'x':0.3, 'top':0.95}
+        pos_hint: {'x':0.35, 'top':0.95}
         valign: 'top'
         
     Button:
-        text: 'Test'
-        text_size: self.width-20, self.height-20
-        size: 75, 30
+        id:captureAndTest
+        text: 'Test Your Age'
+        font_size:24
+        bold: True
+        size: 240, 90
         size_hint: None, None
         haligh: 'left'
-        pos_hint: {'x':0.45, 'top':0.1}
+        pos_hint: {'x':0.349, 'top':0.2}
+        on_release: camera.play = False
+    Button:
+        id:recap
+        text:'Take Another One'
+        font_size:24
+        bold: True
+        size: 250, 90
+        size_hint: None, None
+        pos_hint: {'x':0.505, 'top':0.2}
+        on_release: camera.play = True
+    
+    Camera:
+        id: camera
+        resolution: 1080,1080
 
     
 ''')
 
 
-class BackPacitureLayout(GridLayout):
-    background_image = ObjectProperty(Image(source='bgp.jpg'))
-
 class RootWidget(FloatLayout):
-    pass
+    def capture(self):
+        pass
 
 class MainApp(App):
 
     def build(self):
         temp = RootWidget()
-        self.img1 = Image()
-        temp.add_widget(self.img1)
-        self.capture = cv2.VideoCapture(0)
-        Clock.schedule_interval(self.update, 1.0 / 33.0)
         return temp
 
-    def update(self, dt):
-        # display image from cam in opencv window
-        ret, frame = self.capture.read()
-        # cv2.imshow("CV2 Image", frame)
-        # convert it to texture
-        buf1 = cv2.flip(frame, 0)
-        buf = buf1.tostring()
-        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-        texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-        # display image from the texture
-        self.img1.texture = texture1
+
+
 
 if __name__ == '__main__':
     MainApp().run()
